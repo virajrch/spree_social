d2ca0a7637de03ec3104e8c73ae63d0163943994
virajrch <virajz.c@gmail.com>
2018-10-20 10:02:12 +0530

    Delete add_authentications_to_account_summary.rb

--- app/overrides/add_authentications_to_account_summary.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/users/show',
-  name: 'add_socials_to_account_summary',
-  insert_after: '[data-hook="account_my_orders"]',
-  partial: 'spree/users/social'
-)
